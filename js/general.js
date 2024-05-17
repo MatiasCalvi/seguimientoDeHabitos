a20b92aef78655713630394b8fe5ea6d7be8690b
Matias Calvi <114601077+MatiasCalvi@users.noreply.github.com>
2024-05-17 00:06:57 -0300

    Delete js directory

--- js/general.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("mobile-menu").addEventListener("click", function () {
-  var navList = document.getElementById("nav-list");
-  this.classList.toggle("open");
-  navList.classList.toggle("active");
-});
